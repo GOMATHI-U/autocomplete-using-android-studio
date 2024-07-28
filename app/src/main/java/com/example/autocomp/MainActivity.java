fb7b84387ed7f50aab2d211216aa7923618e3431
GOMATHI-U <uthayakumar2004@gmail.com>
2024-07-28 11:42:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/autocomp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.autocomp;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    String[] language={"c","c++","java",".NET","PHP","ASP.NET","python"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.select_dialog_item,language);
+        AutoCompleteTextView actv=(AutoCompleteTextView) findViewById(R.id.auto);
+        actv.setThreshold(1);
+        actv.setAdapter(adapter);
+    }
+}
